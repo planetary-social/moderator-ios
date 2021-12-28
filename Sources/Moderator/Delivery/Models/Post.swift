93ea0a984ec6b433a8da2c4ecc971b3df776f92a
Martin Dutra <martindsq@hotmail.com>
2021-12-28 17:15:24 -0300

    Add public modifiers to Post properties

--- Sources/Moderator/Delivery/Models/Post.swift
+++ Sources/Moderator/Delivery/Models/Post.swift
@@ -10,41 +10,41 @@ import Foundation
 public extension Moderator {
 
     struct Post {
-        var flags: UInt16
-        var authorHint: UInt64
-        var bl: UInt64
-        var pp: UInt64
-        var garyTimestamp: Int
-        var payloadLenght: Int
-        var payload: Data
-        var keyLen: Int
-        var key: Data?
-        var hint: UInt64
-        var lenghtInBytes: Int
-        var authorAvatar: String?
-        var authorNick: String?
-        var attachmentLink: Data?
-        var perceivedTimestamp: UInt64
-        var likes: Int
-        var shares: Int
+        public var flags: UInt16
+        public var authorHint: UInt64
+        public var bl: UInt64
+        public var pp: UInt64
+        public var garyTimestamp: Int
+        public var payloadLenght: Int
+        public var payload: Data
+        public var keyLen: Int
+        public var key: Data?
+        public var hint: UInt64
+        public var lenghtInBytes: Int
+        public var authorAvatar: String?
+        public var authorNick: String?
+        public var attachmentLink: Data?
+        public var perceivedTimestamp: UInt64
+        public var likes: Int
+        public var shares: Int
 
-        init(flags: UInt16,
-             authorHint: UInt64,
-             bl: UInt64,
-             pp: UInt64,
-             garyTimestamp: Int,
-             payloadLenght: Int,
-             payload: Data,
-             keyLen: Int,
-             key: Data? = nil,
-             hint: UInt64,
-             lenghtInBytes: Int,
-             authorAvatar: String? = nil,
-             authorNick: String? = nil,
-             attachmentLink: Data? = nil,
-             perceivedTimestamp: UInt64,
-             likes: Int,
-             shares: Int) {
+        public init(flags: UInt16,
+                    authorHint: UInt64,
+                    bl: UInt64,
+                    pp: UInt64,
+                    garyTimestamp: Int,
+                    payloadLenght: Int,
+                    payload: Data,
+                    keyLen: Int,
+                    key: Data? = nil,
+                    hint: UInt64,
+                    lenghtInBytes: Int,
+                    authorAvatar: String? = nil,
+                    authorNick: String? = nil,
+                    attachmentLink: Data? = nil,
+                    perceivedTimestamp: UInt64,
+                    likes: Int,
+                    shares: Int) {
             self.flags = flags
             self.authorHint = authorHint
             self.bl = bl
